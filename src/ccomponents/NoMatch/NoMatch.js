bc4110c8d8dcfc25f71d5964b40ca15d18b96d31
joy012 <joy.buet.012@gmail.com>
2020-09-06 18:46:56 +0600

    error fixed

--- src/ccomponents/NoMatch/NoMatch.js
+++ src/ccomponents/NoMatch/NoMatch.js
@@ -5,30 +5,31 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        height: '100vh'
-      }
-    },
-    link: {
-        display: 'block',
-        textAlign: 'center', 
-        textDecoration: 'none'
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
     }
-  }));
+  },
+  link: {
+    display: 'block',
+    textAlign: 'center',
+    textDecoration: 'none'
+  }
+}));
 
 const NoMatch = () => {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <Typography variant="h1" component="h1" style={{textAlign:'center',marginTop: '100px',color:'white'}}>Post Not Found</Typography>
-            <Typography variant="h3" component="h3" style={{textAlign:'center', marginTop: '20px', color:'white'}}>Link is Broken or This Post  is Deleted by User</Typography>
-            <Link className={classes.link} to='/'>
-                <Button variant="contained" color="secondary">Go Back</Button>
-            </Link>
-        </div>
-    );
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <div style={{height:'100vh'}}>
+        <Typography variant="h1" component="h1" style={{ textAlign: 'center', marginTop: '100px', color: 'white' }}>Post Not Found</Typography>
+        <Typography variant="h3" component="h3" style={{ textAlign: 'center', marginTop: '20px', color: 'white' }}>Link is Broken or This Post  is Deleted by User</Typography>
+        <Link className={classes.link} to='/'>
+          <Button variant="contained" color="secondary">Go Back</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NoMatch;
